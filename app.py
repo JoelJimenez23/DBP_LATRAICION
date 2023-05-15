ab795f9053cbecb79ccc78168fc53a8ec2d4cace
Denzel Ysai Bautista Rodriguez <denzel.bautista@utec.edu.pe>
2023-05-15 16:55:36 -0500

    agregue_app.py
    para que cada uno trabaje en su maquina

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask,render_template,request,jsonify
+from flask import Flask,render_template,jsonify,request
 from flask_sqlalchemy import SQLAlchemy
-import uuid 
-import os
+import uuid
+from datetime import datetime
 import sys
+import os
 
-print("Hola Mundo")
+app = Flask(__name__)
 
+# ... Para que cada uno trabaje en su maquina: 
 
-# gola 
+# Obtiene el usuario y la contraseña de las variables de entorno
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
+
+# Construye la URI de la base de datos
+db_uri = f"postgresql://{db_user}:{db_password}@localhost:5432/skinloot"
+
+# Configura la URI en la aplicación Flask
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+
+db = SQLAlchemy(app)
+ALLOWED_EXTENSIONS = {'png','jpeg','jpg'}
